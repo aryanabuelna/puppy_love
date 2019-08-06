a293847c0efecffaed81af253cde64ef0aaed374
Ubuntu <ubuntu@ip-172-31-90-65.ec2.internal>
2019-08-06 23:50:55 +0000

    data storage stuff

--- main.py
+++ main.py
@@ -8,4 +8,19 @@ the_jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
     
-def pfp_url
+def run_query(prof_pic, name, animal, loc, contact, bio_):
+    animal_prof = User_Profile(profile_pic = prof_pic,pet_name = name,animal_type = animal,location = loc, contact_info = contact, bio = bio_)
+    prof_key = animal_prof.put()
+    
+class UserProfileHandler(webapp2.RequestHandler):
+    def get(self):
+   
+        signup_template = the_jinja_env.get_template('templates/sign_up.html')
+        self.response.write(signup_template.render())
+
+
+app = webapp2.WSGIApplication([
+    ('/signup', UserProfileHandler )
+    
+    
+    ], debug=True)
