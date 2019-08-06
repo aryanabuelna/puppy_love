03cb310a441636ea6b6c0464b614fa646d454faf
Ubuntu <ubuntu@ip-172-31-90-65.ec2.internal>
2019-08-06 23:17:01 +0000

    more stuff added to forms

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import webapp2
 import jinja2
 import os
 from models import User_Profile
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+    
+def pfp_url
